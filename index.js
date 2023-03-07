2d270e6be2e62e00e08782672400e0c181762a30
djakss04 <dimas.jakaria.software.solution@gmail.com>
2023-03-07 10:49:29 +0700

    init module

---
+++ index.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = {
+  async check(jid) {
+    var result={
+        verified:false,
+        name: ""
+        picture_url:""
+    }
+    try {
+        var resp = await axios.get("https://wa.me/" + jid)
+        const $ = await cheerio.load(resp.data);
+        const images = $('div > div > img');
+        if (images.length>0) {
+            const urls = images.map((index, element) => $(element).attr('src')).get();
+            const headings = $('div > div > h3');
+            const texts = headings.map((index, element) => $(element).text()).get();
+            result={
+                verified: true,
+                name: texts[0]
+                picture_url:urls[0]
+            }
+        }        
+    } catch (error) {
+        
+    }
+    return result;
+  }
+};
